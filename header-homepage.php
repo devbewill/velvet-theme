ba0d7b91e2bf91e8155e3a06639a7b870fb7f95a
Stefano Perelli <stefano.perelli@nttdata.com>
2014-06-30 18:11:40 +0200

    fixed bg fullscreen

--- header-homepage.php
+++ header-homepage.php
@@ -18,56 +18,15 @@
 <?php wp_head(); ?>
 
 <!-- LOGO SECTION END -->
-</head>
-<style type="text/css">
-@import url(http://fonts.googleapis.com/css?family=Montserrat:400,700);
-@import url(http://fonts.googleapis.com/css?family=Open+Sans:400,700);
-@import url(http://fonts.googleapis.com/css?family=Oswald:300,700);
-@import url(http://fonts.googleapis.com/css?family=Anton);
-
-html,body {
-	height: 100%;
-	margin: 0;
-}
-
-body { 
-background: url('http://i60.tinypic.com/ay252v.jpg') no-repeat 10% 100%; 
--webkit-background-size: cover;
--moz-background-size: cover;
--o-background-size: cover;
-background-size: cover;
-}
-
-.container2 {
-	min-height: 100%;
-  background: linear-gradient(135deg, #723362, #9d223c);
-}
-
-.container {
-	min-height: 100%;
-	background: linear-gradient(to bottom right, rgba(227,5,94, 0.90), rgba(100,1,133, 0.90));
-	border: 5px solid #fff;
-}
-
-.navigation {
-	width: 100%;
-	height: 40px;
-}
-
-.brand {
-	color: #f4f4f4;
-	font-size: 35px;
-	font-family: 'Montserrat';
-	font-weight: 700;
-	padding-left: 15px;
-}
-
-</style>
 <body>
-	<div class="container">
-		<div class="navigation">
-			<span class="brand">DEVBEWILL</span>
+	<section class="image-fullscreen">
+		<div class="home-container">
+			<div class="home-navigation">
+				<span class="home-brand">DEVBEWILL</span>
+			</div>
 		</div>
-	</div>
+	</section>
+</body>
+
 
 
